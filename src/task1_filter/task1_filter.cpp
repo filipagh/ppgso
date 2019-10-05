13a8f8b69187870b5fcee5bce7978f75563d2be2
filipagh <filip11agh@gmail.com>
2019-10-05 16:32:31 +0200

    temp

--- src/task1_filter/task1_filter.cpp
+++ src/task1_filter/task1_filter.cpp
@@ -3,37 +3,46 @@
 //        - Save as result.raw
 #include <fstream>
 #include <iostream>
+#include "../../ppgso/image.h"
+#include "../../ppgso/image_raw.h"
+#include "../../ppgso/image_raw.cpp"
+#include "../../ppgso/image.cpp"
 
 // Size of the framebuffer
 const unsigned int SIZE = 512;
 
-// A simple RGB struct will represent a pixel in the framebuffer
-struct Pixel {
-  // TODO: Define correct data type for r, g, b channel
-};
+//// A simple RGB struct will represent a pixel in the framebuffer
+//struct Pixel {
+//  uint8_t r,g,b;
+//};
+
+void grayScaleImage(ppgso::Image& image) {
+    for (ppgso::Image::Pixel& px : image.getFramebuffer()) {
+        auto magic = static_cast<uint8_t>((px.b * 0.11) + (0.59 * px.g) + (0.3 * px.r));
+        ppgso::Image::updatePixelColor(px,magic,magic,magic);
+    }
+}
+
+void invertImage(ppgso::Image& image) {
+    for (ppgso::Image::Pixel& px : image.getFramebuffer()) {
+        ppgso::Image::updatePixelColor(px,abs(px.r - UINT8_MAX),abs(px.g - UINT8_MAX),abs(px.b - UINT8_MAX));
+    }
+}
 
 int main()
 {
-  // Initialize a framebuffer
-  auto framebuffer = new Pixel[SIZE][SIZE];
+    // TODO: Open file lena.raw (this is 512x512 RAW GRB format)
+    ppgso::Image img = ppgso::image::loadRAW("lena.raw",SIZE,SIZE);
 
-  // TODO: Open file lena.raw (this is 512x512 RAW GRB format)
+    //grayScaleImage(img);
 
-  // TODO: Read data to framebuffer and close the file
+    invertImage(img);
 
-  // Traverse the framebuffer
-  for (unsigned int y = 0; y < SIZE; y++) {
-    for (unsigned int x = 0; x < SIZE; x++) {
-      // TODO: Apply pixel operation
-    }
-  }
 
-  // TODO: Open file result.raw
-  std::cout << "Generating result.raw file ..." << std::endl;
+    std::cout << "Generating result.raw file ..." << std::endl;
 
-  // TODO: Write the framebuffer to the file and close it
+    ppgso::image::saveRAW(img,"result.raw");
 
   std::cout << "Done." << std::endl;
-  delete[] framebuffer;
   return EXIT_SUCCESS;
 }
